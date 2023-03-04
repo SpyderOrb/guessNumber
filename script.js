d9b57a91bf9d275791e646659333045d2716edce
SpyderOrb <alex.plachinda@gmail.com>
2023-03-04 20:55:34 +0100

    added realization of again and check buttons

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// MAIN GAME LOGIC
+// Number between 1 and 20
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+// Event Listener for button Check!
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess >= secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💔 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess <= secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💔 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  // Restoring the initial vaues of the score and number variables.
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  // Restoring the initial conditions of the message, number, score and guess input field.
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.guess').value = '';
+  // Restoring the original background color
+  document.querySelector('body').style.backgroundColor = '#222';
+});
